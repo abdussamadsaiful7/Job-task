2782c3e586be006b35f31b4d24ed585e9c0ea82d
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-07-29 18:42:53 +0600

    added post method

--- src/Components/HomePage/InputPage/InputPage.jsx
+++ src/Components/HomePage/InputPage/InputPage.jsx
@@ -1,14 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 
 const InputPage = () => {
+
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleCheckboxChange = (event) => {
+        setIsChecked(event.target.checked);
+    };
+
+    const handleAddCategory = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const Manufacturing = form.Manufacturing.value;
+        const food = form.food.value;
+        const Furniture = form.Furniture.value;
+        const Machinery = form.Machinery.value;
+        const Maritime = form.Maritime.value;
+        const MetalWorking = form.MetalWorking.value;
+        const CNCMachining = form.CNCMachining.value;
+        const Plastic = form.Plastic.value;
+        const Printing = form.Printing.value;
+        const Wood = form.Wood.value;
+        const Other = form.Other.value;
+        const Information = form.Information.value;
+        const Textile = form.Textile.value;
+        const Service = form.Service.value;
+        const Tourism = form.Tourism.value;
+        const Transport = form.Transport.value;
+
+        const newData = { name, Manufacturing, food, Furniture, Machinery, Maritime, MetalWorking, CNCMachining, Plastic, Printing, Wood, Other, Information, Textile, Service, Tourism, Transport };
+
+       // const newData = {name, category}
+
+        console.log(newData);
+
+        fetch('http://localhost:5080/allData', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(newData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.insertedId) {
+                    toast.success('Add a Product successfully!')
+                }
+                form.reset();
+            })
+    }
+
+
+
+
+
+
     return (
-        <div className='m-36'>
-            <div className='w-1/2'>
+        <div className='md:p-36 p-5'>
+            <form onSubmit={handleAddCategory} className='md:w-1/2'>
                 <div className='mb-6'>
                     <h1 className='mb-2'>Name</h1>
-                    <input type="text" placeholder="Name" className="input input-bordered input-primary w-full" />
+                    <input type="text" placeholder="Name" name='name' className="input input-bordered input-primary w-full" />
                 </div>
-                <div>
+                <div >
                     <h1 className='mb-2'>Sectors</h1>
                     <div
                         className='rounded-lg '
@@ -20,14 +78,16 @@ const InputPage = () => {
                             padding: '10px',
                         }}
                     >
-                        <p>
-                            <select className="w-full">
-                                <option disabled selected>Manufacturing</option>
+                        <p name='category'>
+                            <select defaultValue="Manufacturing" name="Manufacturing" className="w-full">
+                                <option selected disabled >Manufacturing</option>
                                 <option>Construction materials</option>
                                 <option>Electronics and Optics</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Food and Beverage</option>
+
+
+                            <select defaultValue="Food and Beverage" name="food" className="w-full ">
+                                <option  selected disabled >Food and Beverage</option>
                                 <option>Bakery and Confectionary products</option>
                                 <option>Beverage</option>
                                 <option>Fish and Fish products</option>
@@ -36,8 +96,8 @@ const InputPage = () => {
                                 <option>Sweets and snack food</option>
                                 <option>Others</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Furniture</option>
+                            <select defaultValue="Furniture" name="Furniture" className="w-full ">
+                                <option selected disabled >Furniture</option>
                                 <option>Bathroom/Sauna</option>
                                 <option>Bedroom</option>
                                 <option>Children's Room</option>
@@ -47,14 +107,14 @@ const InputPage = () => {
                                 <option>Outdoor</option>
                                 <option>Project Furniture</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Machinery</option>
+                            <select defaultValue="Machinery" name="Machinery" className="w-full ">
+                                <option selected disabled >Machinery</option>
                                 <option>Machinery Components</option>
                                 <option>Machinery equipment/tools</option>
                                 <option>Manufacturing of Machinery</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Maritime</option>
+                            <select defaultValue="Maritime" name="Maritime" className="w-full ">
+                                <option selected disabled >Maritime</option>
                                 <option>Aluminum and steel workboat</option>
                                 <option>Boat/Yacht building</option>
                                 <option>Ship repair and conversion</option>
@@ -62,21 +122,21 @@ const InputPage = () => {
                                 <option>Other</option>
                                 <option>Repair and maintenance service</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>MetalWorking</option>
+                            <select defaultValue="MetalWorking" name="MetalWorking" className="w-full ">
+                                <option selected disabled >MetalWorking</option>
                                 <option>Construction of metal structures</option>
                                 <option>Houses and buildings</option>
                                 <option>Metal products</option>
                                 <option>Metal works</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>CNC-Machining</option>
+                            <select defaultValue="CNC-Machining" name="CNCMachining" className="w-full ">
+                                <option selected disabled >CNC-Machining</option>
                                 <option>Forgings, Fasteners</option>
                                 <option>Gas, Plasma, Laser cutting</option>
                                 <option>MIG, TIG, Aluminum welding</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Plastic and Rubber</option>
+                            <select defaultValue="Plastic and Rubber" name="Plastic" className="w-full ">
+                                <option selected disabled >Plastic and Rubber</option>
                                 <option>Forgings, Fasteners</option>
                                 <option>Packaging</option>
                                 <option>Plastic goods</option>
@@ -86,49 +146,49 @@ const InputPage = () => {
                                 <option>Plastic welding and processing</option>
                                 <option>Plastic profiles</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Printing</option>
+                            <select defaultValue="Printing" name="Printing" className="w-full ">
+                                <option selected disabled >Printing</option>
                                 <option>Advertising</option>
                                 <option>Book/Periodicals printing</option>
                                 <option>Labelling and packing printing</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Textile and Clothing</option>
+                            <select defaultValue="Textile and Clothing" name="Textile" className="w-full ">
+                                <option disabled >Textile and Clothing</option>
                                 <option>Clothing</option>
                                 <option>Book/Periodicals printing</option>
                                 <option>Textile</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Wood</option>
+                            <select defaultValue="Wood" name="Wood" className="w-full ">
+                                <option selected disabled >Wood</option>
                                 <option>Other/wood</option>
                                 <option>Book/Periodicals printing</option>
                                 <option>Wooden building materials</option>
                                 <option>Wooden houses</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Other</option>
+                            <select defaultValue="Other" name="Other" className="w-full ">
+                                <option selected disabled >Other</option>
                                 <option>Creative industries</option>
                                 <option>Energy tecnology</option>
                                 <option>Environment</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Service</option>
+                            <select defaultValue="Service" name="Service" className="w-full ">
+                                <option disabled >Service</option>
                                 <option>Business services</option>
                                 <option>Engineering</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Information Tecnology and Telecommunications</option>
+                            <select defaultValue="Information Technology and Telecommunications" name="Information" className="w-full ">
+                                <option selected disabled >Information Tecnology and Telecommunications</option>
                                 <option>Data processing, Web portals, E-marketing</option>
                                 <option>Programming, Consultancy</option>
                                 <option>Software, Hardware</option>
                                 <option>Telecommunication</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Tourism</option>
+                            <select defaultValue="Tourism" name="Tourism" className="w-full ">
+                                <option selected disabled >Tourism</option>
                                 <option>Translation services</option>
                             </select>
-                            <select className="w-full ">
-                                <option disabled selected>Transport and Logistics</option>
+                            <select defaultValue="Transport and Logistics" name="Transport" className="w-full ">
+                                <option selected disabled >Transport and Logistics</option>
                                 <option>Air</option>
                                 <option>Railway</option>
                                 <option>Road</option>
@@ -138,13 +198,14 @@ const InputPage = () => {
                     </div>
 
                     <div className='flex mt-5'>
-                        <input type="checkbox" name="" id="" />
+                        <input type="checkbox" name="" id="" checked={isChecked}
+                            onChange={handleCheckboxChange} />
                         <p className='ml-2'>Agree to terms</p>
                     </div>
-                    <input className='btn btn-primary btn-sm rounded-sm mt-4' type="submit" value="Save" />
+                    <input className='btn btn-primary btn-sm rounded-sm mt-4' disabled={!isChecked} type="submit" value="Save" />
 
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './HomePage/Home/Home';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Main = () => {
     return (
         <div>
             <Home/>
+            <Toaster/>
         </div>
     );
 };
